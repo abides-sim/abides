3629af414f13c9d2135dc8f02a8187b7e4769643
Mahmoud Mahfouz <mahmoud@mahmouds-mbp.mynet>
2020-03-18 10:47:45 +0000

    pov mm agent fix to query the transacted volume

--- agent/market_makers/POVMarketMakerAgent.py
+++ agent/market_makers/POVMarketMakerAgent.py
@@ -77,12 +77,12 @@ class POVMarketMakerAgent(TradingAgent):
             super().requestDataSubscription(self.symbol, levels=self.subscribe_num_levels,
                                             freq=pd.Timedelta(self.subscribe_freq, unit='ns'))
             self.subscription_requested = True
-            self.getTransactedVolume(self.symbol, lookback_period=self.subscribe_freq)
+            self.get_transacted_volume(self.symbol, lookback_period=self.subscribe_freq)
             self.state = self.initialiseState()
 
         elif can_trade and not self.subscribe:
             self.getCurrentSpread(self.symbol, depth=self.subscribe_num_levels)
-            self.getTransactedVolume(self.symbol, lookback_period=self.wake_up_freq)
+            self.get_transacted_volume(self.symbol, lookback_period=self.wake_up_freq)
             self.initialiseState()
 
     def receiveMessage(self, currentTime, msg):
