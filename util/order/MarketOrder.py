9e15c59c27e41c97db38d94f87078c5554738339
Danial Dervovic <dannydervovic@hotmail.com>
2020-06-11 17:27:07 +0100

    Mm bugfix and Orderbook efficiency improvements (#13)
    * modifications to the subscription API to record the exchange timestamp in the published market data message
    
    * market order changes to account for latency and non-zero jitter
    
    * f string removal from all execution agents
    
    * Custom serialization for order logging and deep copying. (#8)
    
    Co-authored-by: Danial Dervovic <ddervs@googlemail.com>
    
    * Dd serialize orders (#11)
    
    * Custom serialization for order logging and deep copying.
    
    * Orderbook updates (#10)
    
    * Commit makes the following 3 changes to orderbook:
    
      1. Speeds up transacted volume computation
      2. Speeds up conversion of log to DataFrame format
      3. Adds `wide-book` compatibility for `book-freq` != 0
    
    * PR10 fix -- removed redundant print statement
    
    * Order._order_ids uses a set instead of a list to manage order IDs
    
    Co-authored-by: Danial Dervovic <ddervs@googlemail.com>
    
    Co-authored-by: Danial Dervovic <ddervs@googlemail.com>
    
    * Dd orderbook updates (#12)
    
    * Commit makes the following 3 changes to orderbook:
    
      1. Speeds up transacted volume computation
      2. Speeds up conversion of log to DataFrame format
      3. Adds `wide-book` compatibility for `book-freq` != 0
    
    * Custom serialization for order logging and deep copying.
    
    * PR10 fix -- removed redundant print statement
    
    * Order._order_ids uses a set instead of a list to manage order IDs
    
    Co-authored-by: Danial Dervovic <ddervs@googlemail.com>
    
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-20-193.us-east-2.compute.internal>
    Co-authored-by: Mahmoud Mahfouz <mahmoud@mahmouds-mbp.mynet>
    Co-authored-by: Mahmoud Mahfouz <mahmoudmahfouz2005@gmail.com>
    Co-authored-by: Danial Dervovic <ddervs@googlemail.com>

--- util/order/MarketOrder.py
+++ util/order/MarketOrder.py
@@ -1,6 +1,7 @@
 from util.order.Order import Order
 from Kernel import Kernel
 from agent.FinancialAgent import dollarize
+from copy import deepcopy
 
 import sys
 
@@ -9,8 +10,8 @@ silent_mode = False
 
 class MarketOrder(Order):
 
-    def __init__(self, agent_id, time_placed, symbol, quantity, is_buy_order, order_id=None):
-        super().__init__(agent_id, time_placed, symbol, quantity, is_buy_order, order_id)
+    def __init__(self, agent_id, time_placed, symbol, quantity, is_buy_order, order_id=None, tag=None):
+        super().__init__(agent_id, time_placed, symbol, quantity, is_buy_order, order_id=order_id, tag=tag)
 
     def __str__(self):
         if silent_mode: return ''
@@ -22,3 +23,28 @@ class MarketOrder(Order):
     def __repr__(self):
         if silent_mode: return ''
         return self.__str__()
+
+    def __copy__(self):
+        order = MarketOrder(self.agent_id, self.time_placed, self.symbol, self.quantity, self.is_buy_order,
+                            order_id=self.order_id,
+                            tag=self.tag)
+        Order._order_ids.pop()  # remove duplicate agent ID
+        order.fill_price = self.fill_price
+        return order
+
+    def __deepcopy__(self, memodict={}):
+        # Deep copy instance attributes
+        agent_id = deepcopy(self.agent_id, memodict)
+        time_placed = deepcopy(self.time_placed, memodict)
+        symbol = deepcopy(self.symbol, memodict)
+        quantity = deepcopy(self.quantity, memodict)
+        is_buy_order = deepcopy(self.is_buy_order, memodict)
+        order_id = deepcopy(self.order_id, memodict)
+        tag = deepcopy(self.tag, memodict)
+        fill_price = deepcopy(self.fill_price, memodict)
+
+        # Create new order object
+        order = MarketOrder(agent_id, time_placed, symbol, quantity, is_buy_order, order_id=order_id, tag=tag)
+        order.fill_price = fill_price
+
+        return order
